4a093ad7b47131bee89fa6de4543c037759591f1
mahesan luxman <luxman.mahesan@edu.univ-paris13.fr>
2025-12-08 15:34:13 +0100

    first commit

---
+++ src/main/java/com/jobtracker/jobtrackerbackend/JobtrackerBackApplication.java
@@ -0,0 +1,13 @@
+package com.jobtracker.jobtrackerbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JobtrackerBackApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JobtrackerBackApplication.class, args);
+    }
+
+}
